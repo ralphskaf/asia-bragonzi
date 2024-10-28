31efb19247f56d25df0aef31a0a79f4c758bf334
ralphskaf <ralphskaff97@gmail.com>
2024-10-28 18:10:45 +0200

    text

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
   return (
     <>
       <section className="about-section padding-top oh padding-bottom px-md-5">
-        <div className="container-fluid py-5 px-4">
+        <div className="container-fluid py-5 px-1">
           {/* <div className="about-me"> */}
           <div className="">
             <div className="row align-items-center">
@@ -36,7 +36,7 @@ export default function About() {
           <div className="about-content">
             <div className="row">
               <div className="col-md-6 col-12">
-                <div className="content-text">
+                <div className="content-text px-3">
                   <p>
                     She is a center forward who is very active in the team's
                     offensive actions and has a remarkable eye for goal, as well
@@ -51,20 +51,45 @@ export default function About() {
                     className="d-flex align-items-center py-3 mb-3"
                     data-aos="fade-up"
                   >
-                    <div className="img-icon">
+                    {/* <div className="img-icon">
                       <img
                         src={first_icon}
                         alt=""
                         style={{ width: "64px", height: "64px" }}
                       />
-                    </div>
-                    <div className="content-text px-3">
-                      <h6>Biography</h6>
-                      <h6 className="my-text-Sans">Club, Nazionale</h6>
+                    </div> */}
+                    <div className="content-text px-2">
+                      <h6>
+                        <img
+                          src={first_icon}
+                          alt=""
+                          className="me-3"
+                          style={{ width: "64px", height: "64px" }}
+                        />
+                        Biography
+                      </h6>
+                      <h6 className="my-text-Sans">
+                        Asia Bragonzi, born in 2001 in Italy, is a professional
+                        footballer who has garnered attention for her skills as
+                        a forward. Starting her career with Juventus' youth
+                        teams, she later progressed to play for their senior
+                        women’s team, making a mark by scoring five goals in her
+                        initial appearances between the 2018-2019 and 2019-2020
+                        seasons. Following her time with Juventus, she went on a
+                        loan to other clubs, including Hellas Verona, Empoli,
+                        and Sampdoria. In her recent stint with Sampdoria,
+                        Bragonzi notably scored a decisive goal in a game
+                        against Juventus. In January 2024, Asia returned to
+                        Juventus, ready to reinforce the squad under coach Joe
+                        Montemurro amidst ongoing team injuries. She brings back
+                        both experience and talent to the Juventus lineup,
+                        reflecting her commitment to evolving as a player and
+                        contributing to Italian women’s football​ bio
+                      </h6>
                     </div>
                   </div>
                   {/* second icon */}
-                  <div
+                  {/* <div
                     className="d-flex align-items-center py-3 mb-3"
                     data-aos="fade-up"
                   >
@@ -82,24 +107,39 @@ export default function About() {
                         nazionale
                       </h6>
                     </div>
-                  </div>
+                  </div> */}
                   {/* third icon */}
                   <div
                     className="d-flex align-items-center py-3 mb-3"
                     data-aos="fade-up"
                   >
-                    <div className="img-icon">
+                    {/* <div className="img-icon">
                       <img
                         src={third_icon}
                         style={{ width: "64px", height: "64px" }}
                         alt=""
                       />
-                    </div>
+                    </div> */}
                     <div className="content-text px-3">
-                      <h6>Clubs</h6>
+                      <h6>
+                        {" "}
+                        <img
+                          src={third_icon}
+                          style={{ width: "64px", height: "64px" }}
+                          alt=""
+                          className="me-3"
+                        />
+                        Clubs
+                      </h6>
                       <ul className="my-text-Sans px-3">
-                        <li>Club</li>
-                        <li>Nazionale</li>
+                        <li>Juventus F.C. Women (current club)</li>
+                        <li>Inter Milan (youth team)</li>
+                        <li>Hellas Verona </li>
+                        <li>Empoli </li>
+                        <li>Sassuolo </li>
+                        <li>Hellas Verona </li>
+                        <li>Pomigliano </li>
+                        <li>Sampdoria </li>
                       </ul>
                     </div>
                   </div>
@@ -113,7 +153,8 @@ export default function About() {
                         src={imgp3}
                         alt=""
                         className="img-fluid"
-                        style={{ maxHeight: "600px", borderRadius: "10px" }}
+                        style={{ borderRadius: "10px" }}
+                        // style={{ maxHeight: "600px", borderRadius: "10px" }}
                       />
                     </div>
                   </div>
